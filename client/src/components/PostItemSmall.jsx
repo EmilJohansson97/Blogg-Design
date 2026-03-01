bacbfb8992edc0fe3975a66b934914aa904fbc9e
EmilJohansson97 <h24joemi@du.se>
2026-03-01 23:51:41 +0100

    dsa

--- client/src/components/PostItemSmall.jsx
+++ client/src/components/PostItemSmall.jsx
@@ -1,21 +1,56 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Tag from "./Tag.jsx";
+import UserItemSmall from "./UserItemSmall.jsx";
+import { toRelativeDateString, truncate } from "../common/formatHelpers.js";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import placeHolderImage from "../assets/placeholder.png";
+import { Box } from "@mui/system";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 function PostItemSmall({ post }) {
+  const navigate = useNavigate();
   return (
     <>
-      <Link to={`/posts/${post.id}`}>
-        <h3>{post.title}</h3>
-      </Link>
-      <p>
-        Skrivet av:{" "}
-        <Link to={`/users/${post.author.id}/posts`}>
-          {post.author.username}
-        </Link>
-      </p>
-      {post.tags.length > 0 &&
-        post.tags.map((tag) => <Tag key={tag} text={tag} />)}
-      <p>{post.body}</p>
+      <Card variant="outlined" sx={{ mb: 4 }}>
+        <CardHeader
+          title={
+            <Typography variant="h3">
+              <Link to={`/posts/${post.id}`}>{post.title}</Link>
+            </Typography>
+          }
+          subheader={`Skrivet: ${toRelativeDateString(post.createdAt)}`}
+          avatar={<UserItemSmall user={post.author} />}
+        />
+        <CardMedia
+          component="img"
+          height="300"
+          image={post.imageUrl || placeHolderImage}
+          alt={`Bild till ${post.title}`}
+        />
+        <CardContent>
+          <Box mb={2}>
+            {post.tags.length > 0 &&
+              post.tags.map((tag) => <Tag key={tag} text={tag} />)}
+            <Typography variant="body2">{truncate(post.body, 500)}</Typography>
+          </Box>
+        </CardContent>
+        <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button
+            onClick={() => navigate(`/posts/${post.id}`)}
+            endIcon={<ChevronRightIcon />}
+          >
+            Läs mer
+          </Button>
+        </CardActions>
+      </Card>
     </>
   );
 }
